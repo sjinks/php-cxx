98b389b2a706ee6efbe103fe5775c2b74e752bfa
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-12-31 12:39:01 +0200

    Tests

--- test/test_value.cpp
+++ test/test_value.cpp
@@ -12,6 +12,8 @@ protected:
     CommonFixture()
         : m_out(), m_err(), m_sapi(m_out, m_err)
     {
+        m_sapi.setOutputStream(&m_out);
+        m_sapi.setErrorStream(&m_err);
     }
 };
 
@@ -21,57 +23,53 @@ TEST_F(ValueFixture, Initialization)
 {
     m_sapi.run([]() {
         phpcxx::Value undefined;
-        EXPECT_EQ(undefined.type(), phpcxx::Type::Undefined);
-        EXPECT_EQ(undefined.refCount(), 0);
-        EXPECT_EQ(undefined.isReference(), false);
+        EXPECT_EQ(phpcxx::Type::Undefined, undefined.type());
+        EXPECT_EQ(0, undefined.refCount());
+        EXPECT_FALSE(undefined.isReference());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
 
     m_sapi.run([]() {
         phpcxx::Value null(nullptr);
-        EXPECT_EQ(null.type(), phpcxx::Type::Null);
-        EXPECT_EQ(null.refCount(), 0);
-        EXPECT_EQ(null.isReference(), false);
+        EXPECT_EQ(phpcxx::Type::Null, null.type());
+        EXPECT_EQ(0, null.refCount());
+        EXPECT_FALSE(null.isReference());
 
         phpcxx::Value v;
-        EXPECT_EQ(v.type(), phpcxx::Type::Undefined);
+        EXPECT_EQ(phpcxx::Type::Undefined, v.type());
         v = nullptr;
-        EXPECT_EQ(v.type(), phpcxx::Type::Null);
+        EXPECT_EQ(phpcxx::Type::Null, v.type());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
 
     m_sapi.run([]() {
         phpcxx::Value integer(123);
-        EXPECT_EQ(integer.type(), phpcxx::Type::Integer);
-        EXPECT_EQ(integer.refCount(), 0);
-        EXPECT_EQ(integer.isReference(), false);
-        EXPECT_EQ(integer.asLong(), 123);
+        EXPECT_EQ(phpcxx::Type::Integer, integer.type());
+        EXPECT_EQ(0, integer.refCount());
+        EXPECT_FALSE(integer.isReference());
+        EXPECT_EQ(123, integer.asLong());
 
         phpcxx::Value v;
         v = 456;
-        EXPECT_EQ(v.type(), phpcxx::Type::Integer);
-        EXPECT_EQ(v.asLong(), 456);
+        EXPECT_EQ(phpcxx::Type::Integer, v.type());
+        EXPECT_EQ(456, v.asLong());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
 
     m_sapi.run([]() {
         phpcxx::Value integer(123l);
-        EXPECT_EQ(integer.type(), phpcxx::Type::Integer);
-        EXPECT_EQ(integer.refCount(), 0);
-        EXPECT_EQ(integer.isReference(), false);
-        EXPECT_EQ(integer.asLong(), 123);
+        EXPECT_EQ(phpcxx::Type::Integer, integer.type());
+        EXPECT_EQ(0, integer.refCount());
+        EXPECT_FALSE(integer.isReference());
+        EXPECT_EQ(123, integer.asLong());
 
         phpcxx::Value v;
-        v = 456;
-        EXPECT_EQ(v.type(), phpcxx::Type::Integer);
-        EXPECT_EQ(v.asLong(), 456);
-
         v = 456l;
-        EXPECT_EQ(v.type(), phpcxx::Type::Integer);
-        EXPECT_EQ(v.asLong(), 456);
+        EXPECT_EQ(phpcxx::Type::Integer, v.type());
+        EXPECT_EQ(456, v.asLong());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
@@ -80,23 +78,23 @@ TEST_F(ValueFixture, Initialization)
         phpcxx::Value f(123.0f);
         phpcxx::Value d(123.0);
         phpcxx::Value ld(123.0l);
-        EXPECT_EQ(f.type(), phpcxx::Type::Double);
-        EXPECT_EQ(d.type(), phpcxx::Type::Double);
-        EXPECT_EQ(ld.type(), phpcxx::Type::Double);
-        EXPECT_EQ(f.refCount(), 0);
-        EXPECT_EQ(d.refCount(), 0);
-        EXPECT_EQ(ld.refCount(), 0);
-        EXPECT_EQ(f.isReference(), false);
-        EXPECT_EQ(d.isReference(), false);
-        EXPECT_EQ(ld.isReference(), false);
-        EXPECT_EQ(f.asDouble(), 123.0);
-        EXPECT_EQ(d.asDouble(), 123.0);
-        EXPECT_EQ(ld.asDouble(), 123.0);
+        EXPECT_EQ(phpcxx::Type::Double, f.type());
+        EXPECT_EQ(phpcxx::Type::Double, d.type());
+        EXPECT_EQ(phpcxx::Type::Double, ld.type());
+        EXPECT_EQ(0, f.refCount());
+        EXPECT_EQ(0, d.refCount());
+        EXPECT_EQ(0, ld.refCount());
+        EXPECT_FALSE(f.isReference());
+        EXPECT_FALSE(d.isReference());
+        EXPECT_FALSE(ld.isReference());
+        EXPECT_EQ(123.0, f.asDouble());
+        EXPECT_EQ(123.0, d.asDouble());
+        EXPECT_EQ(123.0, ld.asDouble());
 
         phpcxx::Value v;
         v = 456.0l;
-        EXPECT_EQ(v.type(), phpcxx::Type::Double);
-        EXPECT_EQ(v.asDouble(), 456.0);
+        EXPECT_EQ(phpcxx::Type::Double, v.type());
+        EXPECT_EQ(456.0, v.asDouble());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
@@ -104,24 +102,24 @@ TEST_F(ValueFixture, Initialization)
     m_sapi.run([]() {
         phpcxx::Value s("test");
         phpcxx::Value t(phpcxx::string("test"));
-        EXPECT_EQ(s.type(), phpcxx::Type::String);
-        EXPECT_EQ(t.type(), phpcxx::Type::String);
-        EXPECT_EQ(s.refCount(), 1);
-        EXPECT_EQ(t.refCount(), 1);
+        EXPECT_EQ(phpcxx::Type::String, s.type());
+        EXPECT_EQ(phpcxx::Type::String, t.type());
+        EXPECT_EQ(1, s.refCount());
+        EXPECT_EQ(1, t.refCount());
         EXPECT_TRUE(s.isCopyable());
         EXPECT_TRUE(s.isRefcounted());
         EXPECT_FALSE(s.isReference());
         EXPECT_EQ(s.asString(), t.toString());
-        EXPECT_EQ(s.asString(), "test");
+        EXPECT_EQ("test", s.asString());
 
         phpcxx::Value v;
         phpcxx::Value w;
         v = "something";
         w = phpcxx::string("anything");
-        EXPECT_EQ(v.type(), phpcxx::Type::String);
-        EXPECT_EQ(w.type(), phpcxx::Type::String);
-        EXPECT_EQ(v.asString(), "something");
-        EXPECT_EQ(w.asString(), "anything");
+        EXPECT_EQ(phpcxx::Type::String, v.type());
+        EXPECT_EQ(phpcxx::Type::String, w.type());
+        EXPECT_EQ("something", v.asString());
+        EXPECT_EQ("anything",  w.asString());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
@@ -136,15 +134,15 @@ TEST_F(ValueFixture, Initialization)
         phpcxx::Value b = a;
         phpcxx::Value c = a;
 
-        EXPECT_EQ(a.type(), phpcxx::Type::String);
-        EXPECT_EQ(b.type(), phpcxx::Type::String);
-        EXPECT_EQ(c.type(), phpcxx::Type::String);
+        EXPECT_EQ(phpcxx::Type::String, a.type());
+        EXPECT_EQ(phpcxx::Type::String, b.type());
+        EXPECT_EQ(phpcxx::Type::String, c.type());
         EXPECT_FALSE(a.isReference());
         EXPECT_FALSE(b.isReference());
         EXPECT_FALSE(c.isReference());
-        EXPECT_EQ(a.refCount(), 3);
-        EXPECT_EQ(b.refCount(), 3);
-        EXPECT_EQ(c.refCount(), 3);
+        EXPECT_EQ(3, a.refCount());
+        EXPECT_EQ(3, b.refCount());
+        EXPECT_EQ(3, c.refCount());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
@@ -164,14 +162,14 @@ TEST_F(ValueFixture, References)
         phpcxx::Value a(4);
         phpcxx::Value b = phpcxx::Value::createReference(a);
 
-        EXPECT_EQ(a.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(b.type(), phpcxx::Type::Reference);
+        EXPECT_TRUE(a.isReference());
+        EXPECT_TRUE(b.isReference());
         EXPECT_EQ(a.refCount(), b.refCount());
-        EXPECT_EQ(b.refCount(), 2);
-        EXPECT_EQ(a.asLong(), 4);
-        EXPECT_EQ(b.asLong(), 4);
-        EXPECT_EQ(a.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(b.type(), phpcxx::Type::Reference);
+        EXPECT_EQ(2, b.refCount());
+        EXPECT_EQ(4, a.asLong());
+        EXPECT_EQ(4, b.asLong());
+        EXPECT_TRUE(a.isReference());
+        EXPECT_TRUE(b.isReference());
 
         /*
             $a = 5;
@@ -181,12 +179,12 @@ TEST_F(ValueFixture, References)
             b: (refcount=2, is_ref=1)=5
          */
         a = 5;
-        EXPECT_EQ(a.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(b.type(), phpcxx::Type::Reference);
+        EXPECT_TRUE(a.isReference());
+        EXPECT_TRUE(b.isReference());
         EXPECT_EQ(a.refCount(), b.refCount());
-        EXPECT_EQ(b.refCount(), 2);
-        EXPECT_EQ(a.asLong(), 5);
-        EXPECT_EQ(b.asLong(), 5);
+        EXPECT_EQ(2, b.refCount());
+        EXPECT_EQ(5, a.asLong());
+        EXPECT_EQ(5, b.asLong());
 
         /*
             $b = 6;
@@ -196,12 +194,12 @@ TEST_F(ValueFixture, References)
             b: (refcount=2, is_ref=1)=6
          */
         b = 6;
-        EXPECT_EQ(a.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(b.type(), phpcxx::Type::Reference);
+        EXPECT_TRUE(a.isReference());
+        EXPECT_TRUE(b.isReference());
         EXPECT_EQ(a.refCount(), b.refCount());
-        EXPECT_EQ(b.refCount(), 2);
-        EXPECT_EQ(a.asLong(), 6);
-        EXPECT_EQ(b.asLong(), 6);
+        EXPECT_EQ(2, b.refCount());
+        EXPECT_EQ(6, a.asLong());
+        EXPECT_EQ(6, b.asLong());
 
         /*
             $b = $a;
@@ -211,12 +209,12 @@ TEST_F(ValueFixture, References)
             b: (refcount=2, is_ref=1)=6
          */
         b = a;
-        EXPECT_EQ(a.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(b.type(), phpcxx::Type::Reference);
+        EXPECT_TRUE(a.isReference());
+        EXPECT_TRUE(b.isReference());
         EXPECT_EQ(a.refCount(), b.refCount());
-        EXPECT_EQ(b.refCount(), 2);
-        EXPECT_EQ(a.asLong(), 6);
-        EXPECT_EQ(b.asLong(), 6);
+        EXPECT_EQ(2, b.refCount());
+        EXPECT_EQ(6, a.asLong());
+        EXPECT_EQ(6, b.asLong());
     });
     EXPECT_TRUE(m_err.str().empty());
     m_err.str(std::string());
@@ -237,16 +235,18 @@ TEST_F(ValueFixture, References)
         phpcxx::Value c = b;
         a = 1;
 
-        EXPECT_EQ(a.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(b.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(c.type(), phpcxx::Type::Integer);
+        EXPECT_TRUE(a.isReference());
+        EXPECT_TRUE(b.isReference());
+        EXPECT_EQ(phpcxx::Type::Integer, c.type());
         EXPECT_EQ(a.refCount(), b.refCount());
-        EXPECT_EQ(b.refCount(), 2);
-        EXPECT_EQ(c.refCount(), 0);
-        EXPECT_EQ(a.asLong(), 1);
-        EXPECT_EQ(b.asLong(), 1);
-        EXPECT_EQ(c.asLong(), 0);
+        EXPECT_EQ(2, b.refCount());
+        EXPECT_EQ(0, c.refCount());
+        EXPECT_EQ(1, a.asLong());
+        EXPECT_EQ(1, b.asLong());
+        EXPECT_EQ(0, c.asLong());
     });
+    EXPECT_TRUE(m_err.str().empty());
+    m_err.str(std::string());
 
     m_sapi.run([]() {
         /*
@@ -268,14 +268,91 @@ TEST_F(ValueFixture, References)
         b = a;
         c = a;
 
-        EXPECT_EQ(a.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(b.type(), phpcxx::Type::Reference);
-        EXPECT_EQ(c.type(), phpcxx::Type::String);
+        EXPECT_TRUE(a.isReference());
+        EXPECT_TRUE(b.isReference());
+        EXPECT_EQ(phpcxx::Type::String, c.type());
         EXPECT_EQ(a.refCount(), b.refCount());
         EXPECT_EQ(b.refCount(), c.refCount());
-        EXPECT_EQ(c.refCount(), 2);
-        EXPECT_EQ(a.asString(), "s");
-        EXPECT_EQ(b.asString(), "s");
-        EXPECT_EQ(c.asString(), "s");
+        EXPECT_EQ(2, c.refCount());
+        EXPECT_EQ("s", a.asString());
+        EXPECT_EQ("s", b.asString());
+        EXPECT_EQ("s", c.asString());
+    });
+    EXPECT_TRUE(m_err.str().empty());
+    m_err.str(std::string());
+
+    m_sapi.run([this]() {
+        {
+            // arr: (refcount=1, is_ref=0)=array (0 => (refcount=0, is_ref=0)=4)
+            // a: (refcount=2, is_ref=1)=4
+            // b: (refcount=2, is_ref=1)=4
+
+            phpcxx::Value arr;
+            phpcxx::Value a = 4;
+            phpcxx::Value b = phpcxx::Value::createReference(a);
+
+            arr[nullptr] = a;
+
+            EXPECT_EQ(phpcxx::Type::Array, arr.type());
+            EXPECT_EQ(1, arr.refCount());
+            EXPECT_TRUE(a.isReference());
+            EXPECT_TRUE(b.isReference());
+            EXPECT_EQ(2, a.refCount());
+            EXPECT_EQ(2, b.refCount());
+
+            zval* z = arr.getZVal();
+            zval* x = zend_hash_index_find(Z_ARRVAL_P(z), 0);
+            ASSERT_TRUE(x != nullptr);
+            ASSERT_EQ(IS_LONG, Z_TYPE_P(x));
+            EXPECT_EQ(4, Z_LVAL_P(x));
+        }
+
+        EXPECT_TRUE(m_err.str().empty());
+        m_err.str(std::string());
+
+        {
+            // arr: (refcount=1, is_ref=0)=array (0 => (refcount=3, is_ref=1)=4)
+            // a: (refcount=3, is_ref=1)=4
+            // b: (refcount=3, is_ref=1)=4
+
+            phpcxx::Value arr;
+            phpcxx::Value a = 4;
+            phpcxx::Value b = phpcxx::Value::createReference(a);
+
+            arr[nullptr] = phpcxx::Value::createReference(a);
+
+            EXPECT_EQ(phpcxx::Type::Array, arr.type());
+            EXPECT_EQ(1, arr.refCount());
+            EXPECT_TRUE(a.isReference());
+            EXPECT_TRUE(b.isReference());
+            EXPECT_EQ(3, a.refCount());
+            EXPECT_EQ(3, b.refCount());
+
+            zval* z = arr.getZVal();
+            zval* x = zend_hash_index_find(Z_ARRVAL_P(z), 0);
+            ASSERT_TRUE(x != nullptr);
+            ASSERT_EQ(IS_REFERENCE, Z_TYPE_P(x));
+            ASSERT_EQ(IS_LONG, Z_TYPE_P(Z_REFVAL_P(x)));
+            EXPECT_EQ(4, Z_LVAL_P(Z_REFVAL_P(x)));
+        }
+
+        EXPECT_TRUE(m_err.str().empty());
+        m_err.str(std::string());
+        EXPECT_TRUE(m_out.str().empty());
+        m_out.str(std::string());
+
+        {
+            phpcxx::Value a;
+            a["a"]["b"]["c"] = 1;
+            a["a"]["b"]["d"] = 2;
+            a["a"]["e"]      = 3;
+            phpcxx::Value func("var_export");
+            func(a);
+        }
+
+        std::string expected = "array (\n  'a' => \n  array (\n    'b' => \n    array (\n      'c' => 1,\n      'd' => 2,\n    ),\n    'e' => 3,\n  ),\n)";
+        std::string actual   = m_out.str();
+        m_out.str(std::string());
+        EXPECT_EQ(expected, actual);
     });
 }
