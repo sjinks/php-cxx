b5cb782e30b560e14903c64d1d604f3e85b16524
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-01-01 23:56:35 +0200

    Construct zval from bool

--- phpcxx/helpers.h
+++ phpcxx/helpers.h
@@ -10,19 +10,20 @@
 namespace phpcxx {
 
 // See http://loungecpp.wikidot.com/tips-and-tricks:indices
-template <std::size_t... Is>
+template<std::size_t... Is>
 struct indices {};
 
-template <std::size_t N, std::size_t... Is>
+template<std::size_t N, std::size_t... Is>
 struct build_indices : build_indices<N-1, N-1, Is...> {};
 
-template <std::size_t... Is>
+template<std::size_t... Is>
 struct build_indices<0, Is...> : indices<Is...> {};
 
 template<typename ...T>
 using IndicesFor = build_indices<sizeof...(T)>;
 
 static inline void construct_zval(zval& z, std::nullptr_t) { ZVAL_NULL(&z); }
+static inline void construct_zval(zval& z, bool v)         { ZVAL_BOOL(&z, v); }
 
 template<typename T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
 static inline void construct_zval(zval& z, T v) { ZVAL_LONG(&z, v); }
