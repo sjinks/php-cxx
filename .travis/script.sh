935091fcb922a45c51e654d1183c849d2136fad0
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-04-05 14:05:01 +0300

    Repeat tests twice

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,5 +3,5 @@
 set -e
 
 make -j$(getconf _NPROCESSORS_ONLN) COVERAGE=1 PHP_CONFIG=/usr/bin/php-config$PHPVER TESTER_LDLIBS=-lphp$PHPVER
-./.lib/php-cxx-test
-valgrind --error-exitcode=1 --fullpath-after=/php-cxx/ --suppressions=php-cxx-test.sup --gen-suppressions=all --read-inline-info=yes --read-var-info=yes --leak-check=full --show-leak-kinds=all --track-origins=yes ./.lib/php-cxx-test
+./.lib/php-cxx-test --gtest_repeat=2
+valgrind --error-exitcode=1 --fullpath-after=/php-cxx/ --suppressions=php-cxx-test.sup --gen-suppressions=all --read-inline-info=yes --read-var-info=yes --leak-check=full --show-leak-kinds=all --track-origins=yes ./.lib/php-cxx-test --gtest_repeat=2
