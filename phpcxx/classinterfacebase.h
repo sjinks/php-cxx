5b8bb91514eb9f6bd0c345d34b82fd4a75b76d4e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-04-10 04:13:23 +0300

    Add all necessary arguments to addMethod()

--- phpcxx/classinterfacebase.h
+++ phpcxx/classinterfacebase.h
@@ -5,7 +5,6 @@
 #include "classbase.h"
 #include "emallocallocator.h"
 #include "function.h"
-#include "method.h"
 #include "functionhandler.h"
 
 namespace phpcxx {
@@ -22,51 +21,51 @@ protected:
     }
 
     template<MethodPrototypeNN<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 
     template<MethodPrototypeNV<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 
     template<MethodPrototypeVN<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 
     template<MethodPrototypeVV<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 
     template<ConstMethodPrototypeNN<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 
     template<ConstMethodPrototypeNV<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 
     template<ConstMethodPrototypeVN<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 
     template<ConstMethodPrototypeVV<T> Handler>
-    phpcxx::Method addMethod(const char* name)
+    phpcxx::Method addMethod(const char* name, const Arguments& required = {}, const Arguments& optional = {}, bool byRef = false)
     {
-        return phpcxx::createMethod<T, Handler>(name);
+        return phpcxx::createMethod<T, Handler>(name, required, optional, byRef);
     }
 };
 
