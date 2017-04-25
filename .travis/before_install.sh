a330cd85353b6ce841c52b04567cf0f4d861c868
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-04-26 02:37:07 +0300

    There is no ~/.ccache/ccache.conf in Travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,5 +5,4 @@ set -e
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get -qq update
 sudo apt-get -qq install valgrind libgtest-dev libphp$PHPVER-embed php$PHPVER-dev
-cat ~/.ccache/ccache.conf
 cat ~/.ccache/ccache.conf | grep -q 'sloppiness' || echo "sloppiness = pch_defines,time_macros" >> ~/.ccache/ccache.conf
