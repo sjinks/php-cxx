463965c69910d1cf19cb6ebfb754a4aa578b45bb
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-04-26 02:47:01 +0300

    Speed up valgrind

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,4 +5,5 @@ set -e
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get -qq update
 sudo apt-get -qq install valgrind libgtest-dev libphp$PHPVER-embed php$PHPVER-dev
+ccache -V
 cat ~/.ccache/ccache.conf | grep -q 'sloppiness' || echo "sloppiness = pch_defines,time_macros" >> ~/.ccache/ccache.conf
