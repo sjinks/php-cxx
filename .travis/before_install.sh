32489b730b001338b833bd29a15d2afcc2f72bce
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-04-26 02:31:59 +0300

    Travis uses old ccache

---
+++ .travis/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get -qq update
+sudo apt-get -qq install valgrind libgtest-dev libphp$PHPVER-embed php$PHPVER-dev
+cat ~/.ccache/ccache.conf
+cat ~/.ccache/ccache.conf | grep -q 'sloppiness' || echo "sloppiness = pch_defines,time_macros" >> ~/.ccache/ccache.conf
