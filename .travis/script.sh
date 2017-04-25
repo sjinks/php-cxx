b5cc5603510235404e1ba35b68dba36d13e5b132
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-04-26 02:47:01 +0300

    Speed up valgrind

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-make -j$(getconf _NPROCESSORS_ONLN) COVERAGE=1 PHP_CONFIG=/usr/bin/php-config$PHPVER TESTER_LDLIBS=-lphp$PHPVER
-./.lib/php-cxx-test --gtest_repeat=2
-valgrind --error-exitcode=1 --fullpath-after=/php-cxx/ --suppressions=php-cxx-test.sup --gen-suppressions=all --read-inline-info=yes --read-var-info=yes --leak-check=full --show-leak-kinds=all --track-origins=yes ./.lib/php-cxx-test --gtest_repeat=2
+if [ "$NOPCH" = "1" ]; then
+	make -j$(getconf _NPROCESSORS_ONLN) PHP_CONFIG=/usr/bin/php-config$PHPVER TESTER_LDLIBS=-lphp$PHPVER
+	./.lib/php-cxx-test
+else
+	make -j$(getconf _NPROCESSORS_ONLN) COVERAGE=1 PHP_CONFIG=/usr/bin/php-config$PHPVER TESTER_LDLIBS=-lphp$PHPVER
+	./.lib/php-cxx-test --gtest_repeat=2
+	valgrind --error-exitcode=1 --fullpath-after=/php-cxx/ --suppressions=php-cxx-test.sup --gen-suppressions=all --leak-check=full --show-leak-kinds=all --track-origins=yes ./.lib/php-cxx-test --gtest_repeat=2
+fi
