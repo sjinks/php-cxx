79baf03f96304ca9f8c3b31d9ca1c363a16df78a
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-04-29 14:07:59 +0300

    Documentation for PhpCxx\Exception

--- phpcxx/exception.h
+++ phpcxx/exception.h
@@ -8,6 +8,16 @@ extern "C" {
 #include <Zend/zend.h>
 #include <Zend/zend_exceptions.h>
 
+/**
+ * @brief `zend_class_entry*` for PhpCxx\\Exception PHP class
+ * @see http://php.net/manual/en/class.exception.php
+ * @details
+ * ```php
+ * class PhpCxx\Exception extends \Exception
+ * {
+ * }
+ * ```
+ */
 extern ZEND_DLEXPORT zend_class_entry* phpcxx_exception_ce;
 
 #ifdef __cplusplus
--- phpcxx/exception_p.h
+++ phpcxx/exception_p.h
@@ -3,6 +3,13 @@
 
 #include "phpcxx.h"
 
+/**
+ * @internal
+ * @brief Registers PhpCxx\\Exception PHP class
+ * @return Whether registration succeeded
+ * @retval SUCCESS
+ * @retval FAILURE
+ */
 PHPCXX_HIDDEN int setup_phpcxx_exception();
 
 #endif /* PHPCXX_EXCEPTION_P_H */
