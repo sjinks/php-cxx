50b88e3b9f1f27de9b8b25e671baee305bc095fa
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2018-01-03 15:12:09 +0200

    Try 7.2

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,4 +4,4 @@ set -e
 
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get -qq update
-sudo apt-get -qq install valgrind libgtest-dev libphp$PHPVER-embed php$PHPVER-dev
+sudo apt-get -qq install valgrind libgtest-dev libphp$PHPVER-embed php$PHPVER-dev libpcre3-dev
